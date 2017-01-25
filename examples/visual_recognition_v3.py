bd26744236ba4e451bf2b29ab96d058f7f7d8638
Artificial Intelligence Group from UTPL <inteligenciautpl@gmail.com>
2017-01-25 11:05:58 -0500

    Update visual_recognition_v3.py

--- examples/visual_recognition_v3.py
+++ examples/visual_recognition_v3.py
@@ -3,35 +3,27 @@ from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
 import time
-from pprint import pprint
-from decimal import Decimal
 from numpy import array
 
 
 def visual_recognition(foto):
-	visual_recognition = VisualRecognitionV3('2016-05-20', api_key='711fd2ddb6a5fca26191e3d9eddc2f9bf8dc89f6')
+	visual_recognition = VisualRecognitionV3('2016-05-20', api_key='you api key')
 
 	with open(join(dirname(__file__), foto), 'rb') as image_file:
 		    data=json.dumps(visual_recognition.classify(images_file=image_file, threshold=0.1,
-                                            classifier_ids=['Laboratorio50_1483953749']), indent=2, sort_keys=True)
+                                            classifier_ids=['you classifier']), indent=2, sort_keys=True)
 	jsonToPython = json.loads(data)
 	#print(data)
 	datoslista = json.dumps(jsonToPython)
 	data1 = json.loads(datoslista)	 
 	lista = []
 	lista2 = []
-	#nombre=''
-	#score=0.00000
-	#matriz = [nombre, score]
 	for element in data1["images"][0]["classifiers"][0]["classes"]:
 	    lista.append(element["class"])
 	    lista.append(element["score"])
 	    lista2.append(element["score"])
-	    #matriz.append(element["score"],element["class"])
 	print lista   
 	print 'el nro mayor es: ', mayor(lista2)
-	#if (mayor(lista)>=0.997000):
-		#print 'Abrir Puerta'
 	exit()
 #metodo que si el porcetnaje esta dentro del rango para abrir la puerta
 def mayor(lista):
